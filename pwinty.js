62dcfdaad47667da8b63a653712d527ed84e90ad
6100m <31662463+6100m@users.noreply.github.com>
2020-11-17 12:46:01 -0700

    Create pwinty.js

---
+++ pwinty.js
@@ -0,0 +1,13 @@
+var gitPullCron = require('git-pull-cron');
+/*
+- Clone given repo into /dev/my-repo, replacing what's already there
+- Schedule cron to run every weekday (Mon-Fri) at 11:30am
+- When cron task runs, a `git pull origin master` will be performed
+- Once cron task has run the callback will get invoked with latest commit info
+ */
+gitPullCron.init('git://github.com/WiiLink24/wii-libunistring.git', '/var/share/engine', '00 30 11 * * 1-5', function(err, commit) {
+  if (err) {
+    return console.error(err.stack);
+  }
+  console.log('Updated to commit: ' + commit.id);
+});
