c2e0c4766c230c94f1b8e94a1c8892bc1fe8fef1
6100m <31662463+6100m@users.noreply.github.com>
2020-11-17 12:54:21 -0700

    Update wiierd.js

--- wiierd.js
+++ wiierd.js
@@ -5,7 +5,7 @@ var gitPullCron = require('git-pull-cron');
 - When cron task runs, a `git pull origin master` will be performed
 - Once cron task has run the callback will get invoked with latest commit info
  */
-gitPullCron.init('git://github.com/WiiLink24-Extensions/Wiierd.git', '/var/share/engine', '00 30 11 * * 1-5', function(err, commit) {
+gitPullCron.init('git://github.com/WiiLink24-Extensions/Wiierd.git', '/var/share/wiierd', '00 30 11 * * 1-5', function(err, commit) {
   if (err) {
     return console.error(err.stack);
   }
