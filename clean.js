2f5a4b271859569484753e108dbc68ef87d42907
6100m <31662463+6100m@users.noreply.github.com>
2020-11-17 12:58:58 -0700

    Update clean.js

--- clean.js
+++ clean.js
@@ -1,8 +1,30 @@
 const fs = require('fs')
-const path = '.gitignore'
+const path0 = '.gitignore'
+const path1 = 'README.md'
+const path2 = 'LICENSE.md'
+const path3 = '.replit'
 try {
-  fs.unlinkSync(path)
+  fs.unlinkSync(path0)
   //file removed
+} catch(err) {
+  console.error(err)
+}
+try {
+  fs.unlinkSync(path1)
+  //file removed
+} catch(err) {
+  console.error(err)
+}
+try {
+  fs.unlinkSync(path2)
+  //file removed
+} catch(err) {
+  console.error(err)
+}
+try {
+  if (fs.existsSync(path3)) {
+    //file exists
+  }
 } catch(err) {
   console.error(err)
 }
