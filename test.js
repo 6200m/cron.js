da275f883064779cb0ef4e1af0a80c3b2b9ae40a
6100m <31662463+6100m@users.noreply.github.com>
2020-11-17 12:16:30 -0700

    Update test.js

--- test.js
+++ test.js
@@ -3,53 +3,37 @@ var cron = require('cron'),
   git = require('gift'),
   path = require('path'),
   Q = require('bluebird');
-
 var rimraf = Q.promisify(require('rimraf'));
-
 var chai = require('chai'),
   expect = chai.expect,
   should = chai.should();
-
 var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
-
 var sinon = require('sinon');
-
 var gitPullCron = require('../index');
-
 var testDataFolder = path.join(__dirname, 'data'),
   testRepoFolder = path.join(testDataFolder, 'git-pull-cron');
-
 var testRemoteRepo = 'git://github.com/hiddentao/git-pull-cron.git';
-
 var testCronSpecNever = '0 0 0 0 0 *';  // Jan 1st, midnight, i.e. never run
-
-
 exports['git-pull-cron'] = {
   beforeEach: function(done) {
     this.mocker = sinon.sandbox.create();
-
     rimraf(testDataFolder)
       .then(function() {
         return fs.mkdir(testDataFolder);
       })
       .nodeify(done);
   },
-
   afterEach: function(done) {
     this.mocker.restore();
-
     gitPullCron.jobs.forEach(function(cronJob) {
       cronJob.stop();
     });
     gitPullCron.jobs = [];
-
     rimraf(testDataFolder).nodeify(done);
   },
-
   'replaces existing folder with new one': function(done) {
     var dummyFile = path.join(testRepoFolder, 'test.txt');
-
     fs.mkdir(testRepoFolder)
       .then(function createDummyFile() {
         return fs.writeFile(dummyFile, 'hello!');
@@ -63,8 +47,6 @@ exports['git-pull-cron'] = {
       .should.be.rejectedWith(Error)
       .and.notify(done);
   },
-
-
   'actually clones the repo': function(done) {
     return gitPullCron.init(testRemoteRepo, testRepoFolder, testCronSpecNever)
       .then(function checkPkg() {
@@ -78,15 +60,10 @@ exports['git-pull-cron'] = {
       .should.eventually.eql('git-pull-cron')
       .and.notify(done);
   },
-
-
-
   'cron job': {
     beforeEach: function(done) {
       var self = this;
-
       this.updateCb = this.mocker.spy();
-
       gitPullCron.init(
         testRemoteRepo, testRepoFolder, testCronSpecNever, this.updateCb
       )
@@ -95,7 +72,6 @@ exports['git-pull-cron'] = {
         })
         .then(function recordCurrentCommitId() {
           self.repoGit = Q.promisifyAll(git(testRepoFolder));
-
           return self.repoGit.current_commitAsync()
             .then(function gotCommit(commit){
               self.latestCommit = commit;
@@ -103,44 +79,31 @@ exports['git-pull-cron'] = {
         })
         .then(function moveBackOneCommit() {
           self.repoGit.gitAsync = Q.promisify(self.repoGit.git, 'cmd');
-
           return self.repoGit.gitAsync('reset --hard HEAD~1');
         })
         .nodeify(done);
     },
-
     'updates to latest commit': function(done) {
       var self = this;
-
       this.timeout(10000);
-
       this.cronJob._callback();
-
       setTimeout(function() {
-
         self.repoGit.current_commitAsync()
           .then(function gotCommit(commit){
             expect(commit.id).to.eql(self.latestCommit.id);
           })
           .nodeify(done);
-
       }, 5000);
     }
   },
-
-
-
   'adds cron job to list': function(done) {
     expect(gitPullCron.jobs).to.eql([]);
-
     return gitPullCron.init(testRemoteRepo, testRepoFolder, testCronSpecNever)
       .then(function checkJobs() {
         expect(gitPullCron.jobs.length).to.eql(1);
-
         gitPullCron.jobs[0].should.be.instanceOf(cron.CronJob);
         gitPullCron.jobs[0].cronTime.source.should.eql(testCronSpecNever);
       })
       .nodeify(done);
   }
-
 };
