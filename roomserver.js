4490e8bfe0ed4d66e99fd804d1fb56dd138747fc
6100m <31662463+6100m@users.noreply.github.com>
2020-11-18 13:52:35 -0700

    Update roomserver.js

--- roomserver.js
+++ roomserver.js
@@ -1,5 +1,10 @@
 const gitPullCron=require('git-pull-cron');
+/* Original example code by hiddentao */
+/* Example was modified by 6100m */
 /* - Schedules cron to run every weekday (Mon-Fri) at 11:30am */
 gitPullCron.init('git://github.com/WiiLink24/room-server.git','/var/share/roomserver','00 30 11 * * 1-5',function(err,commit) {
   if (err) {
-    return console.error(err.stack);}console.log('Updated to commit: '+commit.id);});
+    return console.error(err.stack);
+  }
+  console.log('Updated to commit: '+commit.id);
+});
