e0b01211430ac8f96095b10261db370d2110fb4c
6100m <31662463+6100m@users.noreply.github.com>
2020-11-18 12:00:46 -0700

    Update generate.js

--- generate.js
+++ generate.js
@@ -7,4 +7,4 @@ const data5 = data0 + data1
 const data6 = data2 + data3
 const data7 = data5 + data6
 const data8 = data7 + data4
-const data9='.npmignore'fs.writeFile(data9, data8, function (err){if (err) throw err; console.log('npmignore file... generated!');});
+const data9='.npmignore';fs.writeFile(data9, data8, function (err){if (err) throw err; console.log('npmignore file... generated!');});
