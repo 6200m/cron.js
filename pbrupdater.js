7c83327402311d3743951cc35c7fb7ee49529f09
6100m <31662463+6100m@users.noreply.github.com>
2020-11-18 14:14:29 -0700

    Create pbrupdater.js

---
+++ pbrupdater.js
@@ -0,0 +1,10 @@
+const gitPullCron=require('git-pull-cron');
+/* Original example code by hiddentao */
+/* Example was modified by 6100m */
+/* Schedules cron to run every weekday (Mon-Fri) at 11:30am */
+gitPullCron.init('git://github.com/TwitchPlaysPokemon/pbrEngine.git','/var/schemas/vulcan','00 30 11 * * 1-5',function(err,commit) {
+  if (err) {
+    return console.error(err.stack);
+  }
+  console.log('Updated to commit: '+commit.id);
+});
