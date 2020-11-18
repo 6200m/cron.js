bdddfe4451266402aae627438de081e73beec781
6100m <31662463+6100m@users.noreply.github.com>
2020-11-18 14:25:50 -0700

    Update tppcore.js

--- tppcore.js
+++ tppcore.js
@@ -2,7 +2,7 @@ const gitPullCron=require('git-pull-cron');
 /* Original example code by hiddentao */
 /* Example was modified by 6100m */
 /* Schedules cron to run every weekday (Mon-Fri) at 11:30am */
-gitPullCron.init('git://github.com/TwitchPlaysPokemon/tpp-core.git','/var/schemas/vulcan','00 30 11 * * 1-5',function(err,commit) {
+gitPullCron.init('git://github.com/TwitchPlaysPokemon/tpp-core.git','/var/other/tpp/1','00 30 11 * * 1-5',function(err,commit) {
   if (err) {
     return console.error(err.stack);
   }
