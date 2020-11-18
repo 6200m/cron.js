ac014e89966af5607b45e0923065e03a4f7d258e
6100m <31662463+6100m@users.noreply.github.com>
2020-11-18 13:05:43 -0700

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,10 +1,10 @@
-const fs=require('fs');const data0 = "node_modules/\n"
-const data1 = "test/\n"
-const data2 = ".travis.yml\n"
-const data3 = ".jshintrc\n"
-const data4 = "Gruntfile.coffee"
-const data5 = data0 + data1
-const data6 = data2 + data3
-const data7 = data5 + data6
-const data8 = data7 + data4
+const fs=require('fs');const data0="node_modules/\n"
+const data1="test/\n"
+const data2=".travis.yml\n"
+const data3=".jshintrc\n"
+const data4="Gruntfile.coffee"
+const data5=data0+data1
+const data6=data2+data3
+const data7=data5+data6
+const data8=data7+data4
 const data9='.npmignore';fs.writeFile(data9, data8, function (err){if (err) throw err; console.log('npmignore file... generated!');});
