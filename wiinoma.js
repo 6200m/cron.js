fa0f0653d6267f9a02dceda3476bb46a79e009ae
6100m <31662463+6100m@users.noreply.github.com>
2020-11-18 13:54:10 -0700

    Update wiinoma.js

--- wiinoma.js
+++ wiinoma.js
@@ -1,5 +1,11 @@
 const gitPullCron=require('git-pull-cron');
+/* Original example code by hiddentao */
+/* Example was modified by 6100m */
 /* - Schedules cron to run every weekday (Mon-Fri) at 11:30am */
 gitPullCron.init('git://github.com/WiiLink24/production-deployment.git','/var/share/wiinoma','00 30 11 * * 1-5',function(err,commit) {
   if (err) {
-    return console.error(err.stack);}console.log('Updated to commit: '+commit.id);});
+    return console.error(err.stack);
+  }
+  console.log('Updated to commit: '+commit.id);
+});
+
