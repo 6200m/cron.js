19a27b0d54e972249b688dbc061c7e6dfc6e58f3
6100m <31662463+6100m@users.noreply.github.com>
2020-11-18 13:57:52 -0700

    Update wiierd.js

--- wiierd.js
+++ wiierd.js
@@ -1,7 +1,7 @@
 const gitPullCron=require('git-pull-cron');
 /* Original example code by hiddentao */
 /* Example was modified by 6100m */
-/* - Schedules cron to run every weekday (Mon-Fri) at 11:30am */
+/* Schedules cron to run every weekday (Mon-Fri) at 11:30am */
 gitPullCron.init('git://github.com/WiiLink24-Extensions/Wiierd.git','/var/share/wiierd','00 30 11 * * 1-5',function(err,commit) {
   if (err) {
     return console.error(err.stack);
