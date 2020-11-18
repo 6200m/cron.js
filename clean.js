e77129b42f65170fb4c344e720b0e45e5b652dd6
6100m <31662463+6100m@users.noreply.github.com>
2020-11-18 12:14:47 -0700

    Update clean.js

--- clean.js
+++ clean.js
@@ -3,43 +3,35 @@ const path0 = '.gitignore'
 const path1 = 'README.md'
 const path2 = 'LICENSE.md'
 const path3 = '.replit'
-setTimeout(function() {
-    try {
-      if (fs.existsSync(path0)) {
-        //file exists
-        fs.unlinkSync(path0)
-      }
-    } catch(err) {
-      console.error(err)
-    }
-}, 3000);
-setTimeout(function() {
-    try {
+try {
+        if (fs.existsSync(path0)) {
+                //file exists
+                fs.unlinkSync(path0)
+        }
+} catch(err) {
+        console.error(err)
+}
+try {
       if (fs.existsSync(path1)) {
-        //file exists
-        fs.unlinkSync(path1)
+                //file exists
+                fs.unlinkSync(path1)
       }
-    } catch(err) {
-      console.error(err)
-    }
-}, 3000);
-setTimeout(function() {
-    try {
+} catch(err) {
+        console.error(err)
+}
+try {
       if (fs.existsSync(path2)) {
-        //file exists
-        fs.unlinkSync(path2)
+                //file exists
+                fs.unlinkSync(path2)
       }
-    } catch(err) {
-      console.error(err)
-    }
-}, 3000);
-setTimeout(function() {
-    try {
+} catch(err) {
+        console.error(err)
+}
+try {
       if (fs.existsSync(path3)) {
-        //file exists
-        fs.unlinkSync(path3)
+                //file exists
+                fs.unlinkSync(path3)
       }
-    } catch(err) {
-      console.error(err)
-    }
-}, 3000);
+} catch(err) {
+        console.error(err)
+}
