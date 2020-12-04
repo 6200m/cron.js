bc6d78a50a773942e77bb99e2b263501aca36335
6100m <31662463+6100m@users.noreply.github.com>
2020-12-04 15:40:38 -0700

    Update packagegen.js

--- packagegen.js
+++ packagegen.js
@@ -1,4 +1,4 @@
-const fs=require('fs');const data00="{\n   \"name\": \"git-pull-cron-mod\",\n"
+const fs=require('fs');const data00="{\n   \"name\": \"git-pull-cron-mod\",\n";
 const data01="   \"version\": \"1.0.0\",\n"
 const data02="   \"description\": \"Modified version of the tool that will git clone a repo into a folder and schedule a cron job to pull updates\",\n"
 const data03="   \"main\": \"index.js\",\n"
@@ -22,19 +22,9 @@ const data20="    \"cron\": \"~1.0.4\",\n"
 const data21="    \"gift\": \"~0.4.1\",\n"
 const data22="    \"bluebird\": \"~1.2.4\"\n   },\n"
 const data23="  \"devDependencies\": {\n"
-const data24="    \"grunt\": \"~0.4.5\",\n"
-const data25="    \"grunt-mocha-test\": \"~0.11.0\",\n"
-const data26="    \"grunt-contrib-jshint\": \"~0.10.0\",\n"
-const data27="    \"matchdep\": \"~0.3.0\",\n"
-const data28="    \"chai\": \"~1.9.1\",\n"
-const data29="    \"sinon\": \"~1.10.2\",\n"
-const data30="    \"then-fs\": \"~1.0.6\",\n"
-const data31="    \"chai-as-promised\": \"~4.1.1\"\n   }\n }"
-const data32=data00+data01+data02+data03+data04+data05+data06+data07+data08+data09
-const data33=data10+data11+data12+data13+data14+data15+data16+data17+data18+data19
-const data34=data20+data21+data22+data23+data24+data25+data26+data27+data28+data29
-const data35=data30+data31
-const data36=data32+data33
-const data37=data34+data35
-const data38=data36+data37
-const data39="package.json";fs.writeFile(data39,data38,function (err){if (err) throw err;console.log(data39+" file... generated!");});
+const data24="    \"grunt\": \"~0.4.5\",\n    \"grunt-mocha-test\": \"~0.11.0\",\n"
+const data25="    \"grunt-contrib-jshint\": \"~0.10.0\",\n    \"matchdep\": \"~0.3.0\",\n"
+const data26="    \"chai\": \"~1.9.1\",\n    \"sinon\": \"~1.10.2\",\n    \"then-fs\": \"~1.0.6\",\n"
+const data27="    \"chai-as-promised\": \"~4.1.1\"\n   }\n }"
+const data28=data00+data01+data02+data03+data04+data05+data06+data07+data08+data09+data10+data11+data12+data13+data14+data15+data16+data17+data18+data19+data20+data21+data22+data23+data24+data25+data26+data27
+const data29="package.json";fs.writeFile(data29,data28,function (err){if (err) throw err;console.log(data29+" file... generated!");});
